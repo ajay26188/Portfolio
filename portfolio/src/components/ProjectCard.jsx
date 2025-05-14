2aeebfd63d186a8e51693608a148619773e10ea6
ajay26188 <ajaysah52@gmail.com>
2025-05-14 15:09:26 +0530

    last commit

---
+++ portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,14 @@
+import './ProjectCard.css';
+
+const ProjectCard = ({ title, description, tech, link }) => {
+  return (
+    <div className="project-card" >
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p><strong>Tech:</strong> {tech}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">View Project</a>
+    </div>
+  );
+};
+
+export default ProjectCard;
